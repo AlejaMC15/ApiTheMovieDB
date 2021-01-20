cbcdb98bddadfca01e058773d8df70f1002133a6
AlejaMC15 <alejandra_meneses@iatech.com.co>
2021-01-20 10:58:22 -0500

    consume service of actress

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Header from './components/header';
 
 function App() {
   const [dataApi, setDataApi] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const url =
-    'https://api.themoviedb.org/3/discover/movie/?api_key=40b79735075518fbb44c19e5036c4a4b&language=es-ES';
+    'https://api.themoviedb.org/3/discover/movie/?api_key=40b79735075518fbb44c19e5036c4a4b&language=es-ES' +
+    '&page=' +
+    currentPage;
 
   const token =
     'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MGI3OTczNTA3NTUxOGZiYjQ0YzE5ZTUwMzZjNGE0YiIsInN1YiI6IjYwMDVmYTgyZTk0MmVlMDAzZWU3NTBkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SW2V6WWQH4AR-lNZmtW1zpRGFqKR6haXXNmxDYqeFw8';
 
   useEffect(() => {
-    const service = () => {
+    const service = (page) => {
       fetch(url, {
         Headers: {
           Autorization: token,
@@ -25,12 +28,69 @@ function App() {
         });
     };
     service();
-  }, []);
+  }, [currentPage]);
 
   return (
     <div className="App bg-dark">
       <Header />
       <Home dataApi={dataApi} />
+      <div className="d-flex justify-content-center ">
+        <nav aria-label="Page navigation example ">
+          <ul className="pagination">
+            <li className="page-item">
+              <button
+                onClick={() => {
+                  if (currentPage > 1) {
+                    setCurrentPage(currentPage - 1);
+                  }
+                }}
+                className="page-link"
+                aria-label="Previous"
+              >
+                <span aria-hidden="true">&laquo;</span>
+              </button>
+            </li>
+            <li className="page-item">
+              <button
+                onClick={() => setCurrentPage(1)}
+                className="page-link"
+                href="#"
+              >
+                1
+              </button>
+            </li>
+            <li className="page-item">
+              <button
+                onClick={() => setCurrentPage(2)}
+                className="page-link"
+                href="#"
+              >
+                2
+              </button>
+            </li>
+            <li className="page-item">
+              <button
+                onClick={() => setCurrentPage(3)}
+                className="page-link"
+                href="#"
+              >
+                3
+              </button>
+            </li>
+            <li className="page-item">
+              <button
+                onClick={() => {
+                  setCurrentPage(currentPage + 1);
+                }}
+                className="page-link"
+                aria-label="Next"
+              >
+                <span aria-hidden="true">&raquo;</span>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,6 +4,8 @@ const Home = ({ dataApi }) => {
   const [getGender, setGetGender] = useState([]);
   const [genderNameList, setGenderNameList] = useState('');
   const [detailsMovies, setDetailsMovies] = useState([]);
+  const [idMovie, setIdMovie] = useState([]);
+  const [namesActress, setNameActress] = useState([]);
 
   const url =
     'https://api.themoviedb.org/3/genre/movie/list?api_key=40b79735075518fbb44c19e5036c4a4b';
@@ -15,7 +17,7 @@ const Home = ({ dataApi }) => {
     const service = () => {
       fetch(url, {
         Headers: {
-          Autorization: token,
+          Authorization: token,
         },
       })
         .then((response) => response.json())
@@ -26,85 +28,85 @@ const Home = ({ dataApi }) => {
     service();
   }, []);
 
+  const listIdMovie = (item) => {
+    fetch(
+      'https://api.themoviedb.org/3/movie/' +
+        item +
+        '/credits?api_key=40b79735075518fbb44c19e5036c4a4b',
+      {
+        Headers: {
+          Authorization: token,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setIdMovie(data.cast);
+      });
+  };
+
+  const getCast = (idMovie) => {
+    if (idMovie.length > 0) {
+      const nameList = [];
+      idMovie &&
+        idMovie.map((name) => {
+          nameList.push(name.name);
+        });
+      setNameActress(nameList.toString());
+    }
+  };
+
   const getNameGender = (idList) => {
-    const name = [];
+    const nameList = [];
     idList &&
       idList.map((id) => {
         const nameGender = getGender?.find(
           (objectGender) => objectGender.id === id
         );
-        name.push(nameGender.name);
+        nameList.push(nameGender.name);
       });
-    setGenderNameList(name.toString());
+    setGenderNameList(nameList.toString());
   };
 
-  console.log(detailsMovies, dataApi);
+  console.log(idMovie);
 
   return (
-    <div>
+    <div className="m-5">
       {dataApi.map((item, index) => {
         return (
-          <div key={index} className="container ">
-            <div className="row">
-              <div className="col m-3">
-                <button
-                  onClick={() => {
-                    getNameGender(item.genre_ids);
-                    setDetailsMovies(item);
-                  }}
-                  type="button"
-                  className="btn btn-primary"
-                  data-bs-toggle="modal"
-                  data-bs-target="#exampleModal"
-                >
-                  <div className="card" style={{ width: '18rem' }}>
-                    <img
-                      src={
-                        item.backdrop_path
-                          ? 'https://image.tmdb.org/t/p/w500' +
-                            item.backdrop_path
-                          : 'Image not available'
-                      }
-                      className="card-img-top"
-                      alt="img_movie"
-                    />
-
-                    <div className="card-body">
-                      <h5 className="card-title text-dark">
-                        {item.original_title}
-                      </h5>
-                      <p className="card-text text-dark">
-                        Date of publication: {item.release_date}
-                      </p>
-                    </div>
-                  </div>
-                </button>
+          <button
+            key={index}
+            onClick={() => {
+              getCast(idMovie);
+              getNameGender(item.genre_ids);
+              setDetailsMovies(item);
+              listIdMovie(item.id);
+            }}
+            type="button"
+            className="btn btn-dark m-1"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            <div className="card m-1" style={{ width: '18rem' }}>
+              <img
+                src={
+                  item.backdrop_path
+                    ? 'https://image.tmdb.org/t/p/w500' + item.backdrop_path
+                    : 'Image not available'
+                }
+                className="card-img-top"
+                alt="img_movie"
+              />
+              <div className="card-body">
+                <h5 className="card-title text-dark">{item.original_title}</h5>
+                <p className="card-text text-dark">
+                  Date of publication: {item.release_date}
+                </p>
               </div>
             </div>
-          </div>
+          </button>
         );
       })}
-      <div className="d-flex justify-content-center ">
-        <nav aria-label="Page navigation example ">
-          <ul class="pagination">
-            <li class="page-item">
-              <a class="page-link" href="#" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-              </a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#">
-                1
-              </a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
       //Modal---------
       <div
         className="modal fade"
@@ -114,12 +116,6 @@ const Home = ({ dataApi }) => {
       >
         <div className="modal-dialog">
           <div className="modal-content">
-            <button
-              type="button"
-              className="btn-close justify-content-right"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
             <div className="modal-header">
               <img
                 src={
@@ -133,6 +129,8 @@ const Home = ({ dataApi }) => {
             <p>{detailsMovies.title}</p>
             <p>Gender: {genderNameList}</p>
             <p>Date: {detailsMovies.release_date}</p>
+            <p className="m-3">Actress: {namesActress}</p>
+
             <div className="modal-footer">
               <button
                 type="button"
